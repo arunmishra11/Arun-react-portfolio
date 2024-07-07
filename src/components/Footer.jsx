348ba144f265c759378acf758c9fd87926e59a07
arunmishra11 <156019606+arunmishra11@users.noreply.github.com>
2024-07-07 18:39:33 -0500

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../styles/footer.css";
+import Resume from "../assets/resume.png";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>Check me out! :</h3>
+
+      <a
+        href="https://github.com/arunmishra11"
+        target="_blank"
+        className="icon-container"
+      >
+        <i className="bi bi-github"></i>
+        <p>GitHub</p>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/arun-mishra-558367110/"
+        target="_blank"
+        className="icon-container"
+      >
+        <i className="bi bi-linkedin"></i>
+        <p>LinkedIn</p>
+      </a>
+
+      <a href="https://x.com/home"
+        target="_blank"
+        className="icon-container"
+      >
+        <i className="bi bi-twitter"></i>
+        <p>Twitter</p>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
